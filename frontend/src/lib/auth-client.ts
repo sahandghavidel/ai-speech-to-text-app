f672f2bdcf80efe537f7fb6bce9292a5d4cf8ec4
Sahand Ghavidel <sahand.gha@gmail.com>
2026-04-18 12:25:30 +0700

    Refactor authClient to use env for BETTER_AUTH_URL configuration

--- frontend/src/lib/auth-client.ts
+++ frontend/src/lib/auth-client.ts
@@ -1,7 +1,8 @@
 import { polarClient } from "@polar-sh/better-auth";
 import { createAuthClient } from "better-auth/react";
+import { env } from "process";
 export const authClient = createAuthClient({
   /** The base URL of the server (optional if you're using the same domain) */
-  baseURL: process.env.BETTER_AUTH_URL || "http://localhost:3000",
+  baseURL: env.BETTER_AUTH_URL,
   plugins: [polarClient()],
 });
